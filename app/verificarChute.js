1807decb34e4f5b5d6131c07e3c53dde72c66dc6
Karlos Kliemann <karloskliemann@gmail.com>
2023-04-04 06:54:12 -0300

    adding replay button

--- app/verificarChute.js
+++ app/verificarChute.js
@@ -19,6 +19,7 @@ function verificaChute(chute) {
         document.body.innerHTML = `
         <h2>Parabéns! Você acertou!</h2>
         <h3>O número secreto era ${numeroSecreto}.</h3>
+        <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `
     } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `
@@ -38,3 +39,9 @@ function chuteInvalido(numero) {
 function chuteFora(numero) {
     return numero > maiorValor || numero < menorValor;
 }
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
